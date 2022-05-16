ea41c9f21fb5746163d0c11cd2bc7417fd8723a9
atlantapatt <99867479+atlantapatt@users.noreply.github.com>
2022-05-15 18:58:37 -0700

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Links from "./Links"
+
+function About(props) {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      {props.bio ? <p>{props.bio}</p> : null }
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links Links = {props.github}  />
+    </div>
+  );
+}
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React from "react";
-import Article from './Article'
-import Header from './Header'
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+
+// pass this data down as props to the child component(s) that need it!
+import user from "../data/user";
+console.log(user);
 
 function App() {
-  return <div className="App">
-    learn react!
-    <Header/>
-    <Article />
-  </div>;
+  return (
+    <div>
+      <NavBar />
+      <Home {...user} />
+      <About {...user} />
+    </div>
+  );
 }
 
 export default App;
